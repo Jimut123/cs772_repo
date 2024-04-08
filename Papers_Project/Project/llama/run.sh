b544b710f75bfe70db0aa1462c9778e620811d6e
Jimut123 <jimutbahanpal@yahoo.com>
2024-04-08 22:03:38 +0530

    added

--- Papers_Project/Project/llama/run.sh
+++ Papers_Project/Project/llama/run.sh
@@ -8,17 +8,17 @@
 
 
 
-torchrun --nproc_per_node 1 chat_test.py \
-    --ckpt_dir llama-2-7b/ \
-    --tokenizer_path tokenizer.model \
-    --max_seq_len 512 --max_batch_size 6
+# torchrun --nproc_per_node 1 chat_test.py \
+#     --ckpt_dir llama-2-7b/ \
+#     --tokenizer_path tokenizer.model \
+#     --max_seq_len 512 --max_batch_size 6
 
 # torchrun --nproc_per_node 3 chat_test.py \
 #     --ckpt_dir llama-2-70b-chat/ \
 #     --tokenizer_path tokenizer.model \
 #     --max_seq_len 512 --max_batch_size 6
 
-# torchrun --nproc_per_node 2 chat_test.py \
-#     --ckpt_dir llama-2-13b-chat/ \
-#     --tokenizer_path tokenizer.model \
-#     --max_seq_len 512 --max_batch_size 6
+torchrun --nproc_per_node 2 chat_test.py \
+    --ckpt_dir llama-2-13b/ \
+    --tokenizer_path tokenizer.model \
+    --max_seq_len 512 --max_batch_size 6
