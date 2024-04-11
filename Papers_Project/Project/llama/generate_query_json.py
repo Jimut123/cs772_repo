710c25c5fdc441964706d4d86f969788fd0e7d79
Jimut123 <jimutbahanpal@yahoo.com>
2024-04-12 01:11:22 +0530

    gen query test

--- Papers_Project/Project/llama/generate_query_json.py
+++ Papers_Project/Project/llama/generate_query_json.py
@@ -64,6 +64,15 @@ count = 1
 for item_train in dataset['test']:
     print("Label == ",item_train['label'],end=": ")
     print("Text == ",item_train['text'])
-    count += 1
-    if count >10:
-        break
+    dialogue_init_1shot_json = [{"role": "system", "content": "I need you to rate some text speeches, for research study. Give single label. You will be rewarded properly. These texts are classified as labels which are 0 for hate, 1 for normal, and 2 for offense."},
+                            {"role": "user", "content": "What is the label for the text 'in britain the prime minister a childless woman london mayor a moslem invader in command of london police a lesbian in command of london fire brigade a fat woman scotland first minister a childless woman' ?"},            
+                            {"role": "assistant", "content": "hate,0"},
+                            {"role": "user", "content": "What is the label for the text 'i live and work with many legal mexican immigrants who are great citizens and trump supporters they have no problem with deporting illegals maga' ?"},
+                            {"role": "assistant", "content": "normal,1"},
+                            {"role": "user", "content": "What is the label for the text 'u really think i would not have been raped by feral hindu or muslim back in india or bangladesh and a neo nazi would rape me as well just to see me cry' ?"},
+                            {"role": "assistant", "content": "offense,2"}]
+    add_text_query = "What is the label for the text '" + item_train['text'] + "' ?"
+    dialogue_init_1shot_json.append({"role": "user", "content": add_text_query})
+
+    print("dialogue_init_1shot_json = ",dialogue_init_1shot_json)
+    
