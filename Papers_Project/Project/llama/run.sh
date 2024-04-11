4483c7a639dcb820c9c8031a40b9dda52be120f3
Jimut123 <jimutbahanpal@yahoo.com>
2024-04-12 01:51:03 +0530

    gen query test

--- Papers_Project/Project/llama/run.sh
+++ Papers_Project/Project/llama/run.sh
@@ -26,7 +26,7 @@
 #     --tokenizer_path tokenizer.model \
 #     --max_seq_len 512 --max_batch_size 6
 
-torchrun --nproc_per_node 2 one_shot_chat.py \
+torchrun --nproc_per_node 1 one_shot_chat.py \
     --ckpt_dir llama-2-7b-chat/ \
     --tokenizer_path tokenizer.model \
     --max_seq_len 512 --max_batch_size 6
