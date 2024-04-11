495b2b5103325fa8c9c74cdfc6f78c821ca7c7c7
Jimut123 <jimutbahanpal@yahoo.com>
2024-04-12 02:42:20 +0530

    gen query test

--- Papers_Project/Project/llama/run.sh
+++ Papers_Project/Project/llama/run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
 # torchrun --nproc_per_node 1 example_chat_completion.py \
 #     --ckpt_dir llama-2-7b-chat/ \
 #     --tokenizer_path tokenizer.model \
 #     --max_seq_len 512 --max_batch_size 6
-
-
 
 # torchrun --nproc_per_node 1 chat_test.py \
 #     --ckpt_dir llama-2-7b/ \
@@ -19,14 +16,28 @@
 #     --max_seq_len 512 --max_batch_size 6
 
 
-############ Works
+############ Works - One shot versions
 
+# 16387*2 VRAM of GPU
 # torchrun --nproc_per_node 2 one_shot_chat.py \
 #     --ckpt_dir llama-2-13b-chat/ \
 #     --tokenizer_path tokenizer.model \
 #     --max_seq_len 512 --max_batch_size 6
 
-torchrun --nproc_per_node 1 one_shot_chat.py \
+# 15984 VRAM of GPU
+# torchrun --nproc_per_node 1 one_shot_chat.py \
+#     --ckpt_dir llama-2-7b-chat/ \
+#     --tokenizer_path tokenizer.model \
+#     --max_seq_len 512 --max_batch_size 6
+
+############ Works - 5 shot versions
+
+# 15984 VRAM of GPU
+torchrun --nproc_per_node 1 five_shot_chat.py \
     --ckpt_dir llama-2-7b-chat/ \
     --tokenizer_path tokenizer.model \
     --max_seq_len 512 --max_batch_size 6
+
+
+
+
