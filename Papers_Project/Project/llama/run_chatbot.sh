929db644fa82d40e06c68b950d28683662670754
Jimut123 <jimutbahanpal@yahoo.com>
2024-05-03 16:02:38 +0530

    one and five shot chat

--- Papers_Project/Project/llama/run_chatbot.sh
+++ Papers_Project/Project/llama/run_chatbot.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 #  Increase this max_seq_len to any other number if there is no context
-torchrun --nproc_per_node 1 llama_2_chatbot.py \
+# torchrun --nproc_per_node 1 llama_2_chatbot.py \
+#     --ckpt_dir llama-2-7b-chat/ \
+#     --tokenizer_path tokenizer.model \
+#     --max_seq_len 9999 --max_batch_size 6
+
+torchrun --nproc_per_node 1 llama_2_chatbot_oneshot.py \
     --ckpt_dir llama-2-7b-chat/ \
     --tokenizer_path tokenizer.model \
     --max_seq_len 9999 --max_batch_size 6
+
+torchrun --nproc_per_node 1 llama_2_chatbot_fiveshot.py \
+    --ckpt_dir llama-2-7b-chat/ \
+    --tokenizer_path tokenizer.model \
+    --max_seq_len 9999 --max_batch_size 6
+
+
