23aece59afacedf711f814a475b2a1b054d4b849
Jimut123 <jimutbahanpal@yahoo.com>
2024-05-05 00:34:09 +0530

    added

--- Papers_Project/Project/llama/run_chatbot.sh
+++ Papers_Project/Project/llama/run_chatbot.sh
@@ -22,16 +22,16 @@
 #     --max_seq_len 9999 --max_batch_size 6
 
 #  Increase this max_seq_len to any other number if there is no context
-torchrun --nproc_per_node 1 CoT_llama_2_oneshot_train_example.py \
-    --ckpt_dir llama-2-7b-chat/ \
-    --tokenizer_path tokenizer.model \
-    --max_seq_len 9999 --max_batch_size 6
-
-# #  Increase this max_seq_len to any other number if there is no context
-# torchrun --nproc_per_node 1 CoT_llama_2_fiveshot_train_example.py \
+# torchrun --nproc_per_node 1 CoT_llama_2_oneshot_train_example.py \
 #     --ckpt_dir llama-2-7b-chat/ \
 #     --tokenizer_path tokenizer.model \
 #     --max_seq_len 9999 --max_batch_size 6
+
+# #  Increase this max_seq_len to any other number if there is no context
+torchrun --nproc_per_node 1 CoT_llama_2_fiveshot_train_example.py \
+    --ckpt_dir llama-2-7b-chat/ \
+    --tokenizer_path tokenizer.model \
+    --max_seq_len 9999 --max_batch_size 6
 
 
 # #  Increase this max_seq_len to any other number if there is no context
